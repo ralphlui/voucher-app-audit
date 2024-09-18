305dc1c915920dcd433775316f513d6f41bff086
ThazinWai63 <e1332403@u.nus.edu>
2024-09-18 23:54:16 +0800

    C01 -  committing cleaned initial project

---
+++ src/test/java/sg/edu/nus/iss/voucher/audit/logger/VoucherAppAuditApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.voucher.audit.logger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoucherAppAuditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
